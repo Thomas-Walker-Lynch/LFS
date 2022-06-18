1240f40ad9c4f70d665314e53ebcf1a94172a264
Thomas Walker Lynch <thomas.walker.lynch@gmail.com>
2022-06-18 23:24:37 +0000

    moved /LFS11 to /mnt/buiild, prefixed vars LFS, M2_ROOT->M2_FILESYSTEM

--- user/deprecated/umount_code.sh
+++ user/deprecated/umount_code.sh
@@ -1,4 +1,4 @@
-  test -d "$M2_ROOT/dev" && find "$M2_ROOT/dev"  -mindepth 1 -type d -exec umount {} >& /dev/null '&&' echo "unmounted:" {} \;
-  test -d "$M2_ROOT/run" && umount "$M2_ROOT/run" && echo 'unmounted "$M2_ROOT/run"'
-  test -d "$M2_ROOT/proc" && umount "$M2_ROOT/proc" && echo 'unmounted "$M2_ROOT/proc"'
+  test -d "$M2_FILESYSTEM/dev" && find "$M2_FILESYSTEM/dev"  -mindepth 1 -type d -exec umount {} >& /dev/null '&&' echo "unmounted:" {} \;
+  test -d "$M2_FILESYSTEM/run" && umount "$M2_FILESYSTEM/run" && echo 'unmounted "$M2_FILESYSTEM/run"'
+  test -d "$M2_FILESYSTEM/proc" && umount "$M2_FILESYSTEM/proc" && echo 'unmounted "$M2_FILESYSTEM/proc"'
   # probably will not find anything
